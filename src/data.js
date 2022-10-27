496b4c14be4e0b3f5447011e642ece80eebb0c20
shwwwna <107602771+shwwwna@users.noreply.github.com>
2022-10-27 16:46:09 +0800

    Changed image link of France

--- src/data.js
+++ src/data.js
@@ -1,35 +1,37 @@
+import france from "./assets/france.jpg";
+
 export default [
-  {
-    id: 1,
-    title: "Dongdaemun Design Plaza",
-    location: "South Korea",
-    googleMapsUrl: "https://goo.gl/maps/HHsJaUw5bxSvggR28",
-    startDate: "Jun 7 2015",
-    endDate: "Jun 14 2015",
-    description:
-      'The Dongdaemun Design Plaza, abbreviated as DDP, is a major urban development landmark with a distinctively neofuturistic design characterized by the "powerful, curving forms of elongated structures". ',
-    imageUrl: "/src/assets/korea.jpg",
-  },
-  {
-    id: 2,
-    title: "Eiffel Tower",
-    location: "France",
-    googleMapsUrl: "https://goo.gl/maps/avBXyB7VhSmTgnuw9",
-    startDate: "Sep 20 2017",
-    endDate: "Sep 27 2017",
-    description:
-      "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
-    imageUrl: "/src/assets/france.jpg",
-  },
-  {
-    id: 3,
-    title: "Marina Bay Waterfront Promenade",
-    location: "Singapore",
-    googleMapsUrl: "https://goo.gl/maps/Ma6hG4N69HJBgCfj8",
-    startDate: "July 20 2018",
-    endDate: "July 24 2018",
-    description:
-      "Waterside path surrounded by skyscrapers, with a nightly light show & the striking Helix footbridge.",
-    imageUrl: "/src/assets/singapore.jpg",
-  },
+	{
+		id: 1,
+		title: "Dongdaemun Design Plaza",
+		location: "South Korea",
+		googleMapsUrl: "https://goo.gl/maps/HHsJaUw5bxSvggR28",
+		startDate: "Jun 7 2015",
+		endDate: "Jun 14 2015",
+		description:
+			'The Dongdaemun Design Plaza, abbreviated as DDP, is a major urban development landmark with a distinctively neofuturistic design characterized by the "powerful, curving forms of elongated structures". ',
+		imageUrl: "/src/assets/korea.jpg",
+	},
+	{
+		id: 2,
+		title: "Eiffel Tower",
+		location: "France",
+		googleMapsUrl: "https://goo.gl/maps/avBXyB7VhSmTgnuw9",
+		startDate: "Sep 20 2017",
+		endDate: "Sep 27 2017",
+		description:
+			"The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
+		imageUrl: { france },
+	},
+	{
+		id: 3,
+		title: "Marina Bay Waterfront Promenade",
+		location: "Singapore",
+		googleMapsUrl: "https://goo.gl/maps/Ma6hG4N69HJBgCfj8",
+		startDate: "July 20 2018",
+		endDate: "July 24 2018",
+		description:
+			"Waterside path surrounded by skyscrapers, with a nightly light show & the striking Helix footbridge.",
+		imageUrl: "/src/assets/singapore.jpg",
+	},
 ];
